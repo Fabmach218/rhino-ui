7009789d16de4be4bf010e2dbd7090077ccfc99d
Fabio Marangon <fabio_marangon@usmp.pe>
2024-03-07 09:18:48 -0500

    accordion component working

--- apps/site/docs/Components/Accordion/Default.js
+++ apps/site/docs/Components/Accordion/Default.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Accordion } from '../../../../../packages/components/src/components/accordion/accordion';
+import { Accordion } from '@rhino-ui/components/src/components/accordion/accordion';
 
 export default function Default() {
     const [openPanels, setOpenPanels] = useState({
--- apps/site/docs/Components/Accordion/LeftCaret.js
+++ apps/site/docs/Components/Accordion/LeftCaret.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Accordion } from '../../../../../packages/components/src/components/accordion/accordion';
+import { Accordion } from '@rhino-ui/components/src/components/accordion/accordion';
 
 export default function LeftCaret() {
     const [openPanels, setOpenPanels] = useState({
