4cabfe8f174079b10f01ae97c6d6307998d67fcd
Fabio Marangon <fabio_marangon@usmp.pe>
2024-03-13 16:23:29 -0500

    add time-picker component

---
+++ apps/site/docs/components/stories/time-picker.js
@@ -0,0 +1,144 @@
+import { useState } from 'react';
+import { Box } from "@rhino-ui/components/src/components/box/box";
+import { TimePicker } from "@rhino-ui/components/src/components/time-picker/time-picker"; 
+
+export function Default() {
+    const [value, setValue] = useState(null);
+      return (
+        <Box height="360px">
+          <TimePicker
+            id="defaultTimePicker"
+            name="defaultTimePicker"
+            onChange={event => {
+              setValue(event.target.value);
+            }}
+            label="Pick a Time"
+            value={value}
+          />
+        </Box>
+      );
+}
+
+export function WithInterval() {
+    return (
+        <Box height="360px">
+          <TimePicker
+            id="intervalTimePicker"
+            name="intervalTimePicker"
+            onChange={() => {}}
+            label="Pick a Time"
+            interval={3600}
+          />
+        </Box>
+      );
+}
+
+export function WithStartEnd() {
+    return (
+        <Box height="360px">
+          <TimePicker
+            id="startEnd"
+            name="startEnd"
+            onChange={() => {}}
+            label="Pick a Time"
+            startTime={{ hour: 9, minute: 0 }}
+            endTime={{ hour: 15, minute: 31 }}
+          />
+        </Box>
+      );
+}
+
+export function WithCustomDate() {
+    return (
+        <Box height="360px">
+          <TimePicker
+            id="customDate"
+            name="customDate"
+            onChange={() => {}}
+            label="Pick a Time"
+            startTime={{ hour: 9, minute: 0 }}
+            endTime={{ hour: 15, minute: 31 }}
+            dateDisplayOptions={{ hour12: false, hour: '2-digit', minute: '2-digit' }}
+          />
+        </Box>
+      );
+}
+
+export function OpenMenu() {
+    const [value, setValue] = useState(null);
+      return (
+        <Box height="260px">
+          <TimePicker
+            id="openMenu"
+            name="openMenu"
+            onChange={event => {
+              setValue(event.target.value);
+            }}
+            label="Pick a Time"
+            value={value}
+            menuIsOpen
+            interval={3600}
+            startTime={{ hour: 9, minute: 0 }}
+            endTime={{ hour: 13, minute: 1 }}
+          />
+        </Box>
+      );
+}
+
+export function HelpText() {
+    const [value, setValue] = useState(null);
+      return (
+        <Box height="360px">
+          <TimePicker
+            id="helpText"
+            name="helpText"
+            onChange={event => {
+              setValue(event.target.value);
+            }}
+            label="Pick a Time"
+            value={value}
+            helpText="pick a time, any time..."
+          />
+        </Box>
+      );
+}
+
+export function Sizes() {
+    const [value, setValue] = useState();
+      const [value1, setValue1] = useState();
+      const [value2, setValue2] = useState();
+      return (
+        <Box gap="md" height="360px">
+          <TimePicker
+            id="smTimePicker"
+            name="smTimePicker"
+            onChange={event => {
+              setValue(event.target.value);
+            }}
+            label="Small"
+            value={value}
+            size="sm"
+          />
+          <TimePicker
+            id="mdTimePicker"
+            name="mdTimePicker"
+            onChange={event => {
+              setValue1(event.target.value);
+            }}
+            label="Medium"
+            value={value1}
+            size="md"
+          />
+          <TimePicker
+            id="lgTimePicker"
+            name="lgTimePicker"
+            onChange={event => {
+              setValue2(event.target.value);
+            }}
+            label="Large"
+            value={value2}
+            size="lg"
+          />
+        </Box>
+      );
+}
